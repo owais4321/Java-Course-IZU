e73506bcfa48b31307aee05c5d542802df630ef2
unknown <owais.leghari@hotmail.com>
2024-11-12 00:36:56 +0500

    Assignment and Coded Added

---
+++ Code/Class1.java
@@ -0,0 +1,29 @@
+class Class1{
+    public static void main(String arg[]){
+        System.out.println("Hellow World");
+                // Integer types
+        byte byteVar = 127;
+        short shortVar = 32767;
+        int intVar = 2147483647;
+        long longVar = 9223372036854775807L;
+        // Floating-point types
+        float floatVar = 3.14159f;
+        double doubleVar = 3.14159265358979;
+        // Boolean type
+        boolean booleanVar = true;
+        // Character type
+        char charVar = 'A';
+        // Printing the values
+        System.out.println("Byte value: " + byteVar);
+        System.out.println("Short value: " + shortVar);
+        System.out.println("Int value: " + intVar);
+        System.out.println("Long value: " + longVar);
+        System.out.println("Float value: " + floatVar);
+        System.out.println("Double value: " + doubleVar);
+        System.out.println("Boolean value: " + booleanVar);
+        System.out.println("Char value: " + charVar);
+
+    }
+}
+
+
